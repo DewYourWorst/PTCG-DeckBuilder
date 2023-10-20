a862b343d2be493e950da222ea543a95163d16a6
Nicolas Alonzo <nicalonzo80@yahoo.com>
2023-10-20 10:53:17 -0500

    homepage outline

---
+++ src/Homepage.js
@@ -0,0 +1,18 @@
+
+
+const Homepage = () => {
+
+    return(
+        <>
+            <nav>
+                <div>CardCommander</div>
+                <div>Sets</div>
+                <div>Pre-Made Decks</div>
+                <div>Log-in</div>
+            </nav>
+
+            <h1>Welcome to my Pokemon Deck Builder!</h1>
+            <input type:text placeholder="Search for a Card to start your deck!"></input>
+        </>
+    )
+}
